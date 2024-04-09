36997063a6694aca4d7abe4d777465715b6f431c
Diana Rukavishnikova <psihomatika@gmail.com>
2024-04-09 09:28:42 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import './App.css'
-import Navigation from './components/navigation/Navigation.jsx';
-import Footer from './components/footer/Footer.jsx';
-import AllWordsButton from './components/allwordsbutton/AllWordsButton';
-import LoginForm from './components/navigation/loginform/LoginForm.jsx';
-import RegisterForm from './components/navigation/registerform/RegisterForm.jsx';
-import Main from './components/main/Main.jsx';
-import NewWord from './components/newword/NewWord.jsx';
-import Missing from './components/missingpage/Missing.jsx';
-import { BrowserRouter as Router, Routes, Route } from'react-router-dom';
+import "./App.css";
+import Navigation from "./components/navigation/Navigation.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import AllWordsButton from "./components/allwordsbutton/AllWordsButton";
+import LoginForm from "./components/navigation/loginform/LoginForm.jsx";
+import RegisterForm from "./components/navigation/registerform/RegisterForm.jsx";
+import Main from "./components/main/Main.jsx";
+import NewWord from "./components/newword/NewWord.jsx";
+import Missing from "./components/missingpage/Missing.jsx";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-
-
-function App(){
-
+function App() {
   return (
     <Router>
       <div>
-      <Navigation />
+        <Navigation />
         <Routes>
-          <Route path="/" element={ <AllWordsButton /> } />
-          <Route path="/main" element={ <Main /> } />
-          <Route path="/newword" element={ <NewWord /> } />
-          <Route path="/registerform" element={ <RegisterForm /> } />
-          <Route path="/loginform" element={ <LoginForm /> } />
-          <Route path="*" element={ <Missing/> } />
+          <Route path="/" element={<AllWordsButton />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/newword" element={<NewWord />} />
+          <Route path="/registerform" element={<RegisterForm />} />
+          <Route path="/loginform" element={<LoginForm />} />
+          <Route path="*" element={<Missing />} />
         </Routes>
-        <Footer /> 
+        <Footer />
       </div>
     </Router>
   );
-};
+}
 
 export default App;
